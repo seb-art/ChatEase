27af1f45441894a76eeeed20cdac320f8d074968
seb-art <kimanzikims300@gmsil.com>
2023-12-20 13:40:13 +0300

    backend completed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,5 +2,6 @@ const {register} = require("../controllers/userController")
 
 const router = require("express").Router();
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
