25e4db5a34e9270ed5d44bcd590585c9e0c74e69
seb-art <kimanzikims300@gmail.com>
2024-02-02 18:03:19 +0300

    some fixes

--- backend/index.js
+++ backend/index.js
@@ -1,51 +1,51 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const authRoutes = require("./routes/auth");
-const messageRoutes = require("./routes/messages");
-const app = express();
-const socket = require("socket.io");
-require("dotenv").config();
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const authRoutes = require('./routes/auth')
+const messageRoutes = require('./routes/messages')
+const app = express()
+const socket = require('socket.io')
+require('dotenv').config()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   })
   .then(() => {
-    console.log("The Database was successfuly connected");
+    console.log('The Database was successfuly connected')
   })
-  .catch((err) => {
-    console.log(err.message);
-  });
+  .catch(err => {
+    console.log(err.message)
+  })
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
+app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
 
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
-);
+)
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
-    credentials: true,
-  },
-});
+    origin: 'http://localhost:3000',
+    credentials: true
+  }
+})
 
-global.onlineUsers = new Map();
-io.on("connection", (socket) => {
-  global.chatSocket = socket;
-  socket.on("add-user", (userId) => {
-    onlineUsers.set(userId, socket.id);
-  });
+global.onlineUsers = new Map()
+io.on('connection', socket => {
+  global.chatSocket = socket
+  socket.on('add-user', userId => {
+    onlineUsers.set(userId, socket.id)
+  })
 
-  socket.on("send-msg", (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
+  socket.on('send-msg', data => {
+    const sendUserSocket = onlineUsers.get(data.to)
     if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+      socket.to(sendUserSocket).emit('msg-recieve', data.msg)
     }
-  });
-});
+  })
+})
